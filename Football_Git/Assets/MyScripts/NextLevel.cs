12bdd876a3c40ef9a76c0ece65ad8b182da96637
Mahdi-PC\Mahdi <moradimahdi503@gmail.com>
2020-11-29 15:48:33 +0330

    Change

---
+++ Football_Git/Assets/MyScripts/NextLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextLevel : MonoBehaviour
+{
+    public Canvas nextCanvas;
+    public void LoadNextCanvas()
+    {
+        nextCanvas.GetComponent<Canvas>().enabled = true;
+    }
+}
